5a5d22aaddff59e45d0967b6605ee7ed7eb7f9b2
Allan <kipropsallan@gmail.com>
2022-08-22 15:08:09 +0300

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
+require 'pry'
+
 ## code your solution here. 
+class = class 
+    attr_accessor :cat
+    def initialize(cat)
+        @cat = cat
+    end
+    def meow
+        puts "meow"
+    end
+    
+    def cat
+        @cat
+    end
+end
+
